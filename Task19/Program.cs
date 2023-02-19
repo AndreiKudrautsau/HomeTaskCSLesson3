ff19cd78a9b36199cfd957c641c4181b56613eb8
Andrei Kudrautsau <kavalerand@gmail.com>
2023-02-19 10:53:18 +0300

    added Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,3 +4,14 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Введите пятизначное натуральное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int number1  = Round (number);
+if (number1 == 0) Console.WriteLine($"Число {number} является палиндромом");
+else Console.WriteLine($"Число {number} НЕ является палиндромом");
+
+int Round (int num)
+{
+    return (num % 10) *10000 + ((num / 10) % 10) * 1000 + (((num / 100) % 10) *100) + ((num / 1000) % 10) * 10 + num / 10000 - num;  
+}
